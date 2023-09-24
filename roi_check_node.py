a82085f650dbf59786497944bf26c806cb995e78
Seunmul <geonhab504@gmail.com>
2023-09-25 02:07:18 +0900

    feat : finish roi_check_node.py

--- roi_check_node.py
+++ roi_check_node.py
@@ -1,5 +1,15 @@
 #! /usr/bin/env python3
 # -*- coding:utf-8 -*-
+
+# ==============================================================================
+# Title: roi_check_node.py
+# Description: roi check node
+# Author: @GeonhaPark
+# Date: 2022-10-19
+# Version: 0.1
+# Usage: roslaunch roi_check_node roi_check_node.launch
+# ==============================================================================
+
 import cv2
 import numpy as np
 
@@ -8,11 +18,13 @@ from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 
 cv_bridge = CvBridge()
-class ROI_C:
+
+
+class ROI_CHECK:
     def __init__(self, ref_image=None):
-        rospy.init_node('color_filter', anonymous=True)
+        rospy.init_node("color_filter", anonymous=True)
         # image BGR->HSV로 변환
-        if(ref_image is None):
+        if ref_image is None:
             # 이미지 생성 (검은 배경)
             ref_image = np.ones((800, 400, 3), np.uint8)
 
@@ -48,10 +60,12 @@ class ROI_C:
             print("purple:   ", purple_hsv)
             print("lightblue:", lightblue_hsv)
             print("white:    ", white_hsv)
-        self.ref_image = ref_image
-        self.ref_image_hsv = cv2.cvtColor(self.ref_image, cv2.COLOR_BGR2HSV)
         self.image = ref_image
+        self.ref_image = ref_image
+        self.editted_image = ref_image
         self.image_hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+        self.ref_image_hsv = self.image_hsv
+        self.editted_image_hsv = self.image_hsv
         self.lhue = 0
         self.lsat = 0
         self.lval = 0
@@ -60,60 +74,115 @@ class ROI_C:
         self.hval = 0
         self.update_color()
         pass
-    
-    def image_callback(self,image):
+
+    def image_callback(self, image):
         cimg = cv_bridge.imgmsg_to_cv2(image, "bgr8")
         self.image = cimg
-        
-        
+
+        ##########=====CHECK+++ROI====##########
         # transformation
-        editted_tmp_img = cv2.resize(cimg, None, fx=0.6, fy=0.6,
-                                        interpolation=cv2.INTER_CUBIC)
-        #print editted_tmp_img.shape
+        editted_tmp_img = cv2.resize(
+            self.image, None, fx=0.6, fy=0.6, interpolation=cv2.INTER_CUBIC
+        )
         rows, cols, ch = editted_tmp_img.shape
-        pts1 = np.float32([[10,80],[0,130],[180,80],[190,130]]) #[[15,60],[5,130],[180,60],[190,130]]
+
+        #### ADD LINES FOR pts1 ####
+        dot1 = np.array([10, 80])
+        dot2 = np.array([0, 130])
+        dot3 = np.array([180, 80])
+        dot4 = np.array([190, 130])
+        cv2.circle(
+            self.image, (dot1[0], dot1[1]), radius=5, color=(255, 0, 0), thickness=-1
+        )
+        cv2.circle(
+            self.image, (dot2[0], dot2[1]), radius=5, color=(255, 0, 0), thickness=-1
+        )
+        cv2.circle(
+            self.image, (dot3[0], dot3[1]), radius=5, color=(255, 0, 0), thickness=-1
+        )
+        cv2.circle(
+            self.image, (dot4[0], dot4[1]), radius=5, color=(255, 0, 0), thickness=-1
+        )
+        cv2.line(
+            self.image,
+            (dot1[0], dot1[1]),
+            (dot2[0], dot2[1]),
+            color=(255, 255, 255),
+            thickness=1,
+        )
+        cv2.line(
+            self.image,
+            (dot2[0], dot2[1]),
+            (dot4[0], dot4[1]),
+            color=(255, 255, 255),
+            thickness=1,
+        )
+        cv2.line(
+            self.image,
+            (dot4[0], dot4[1]),
+            (dot3[0], dot3[1]),
+            color=(255, 255, 255),
+            thickness=1,
+        )
+        cv2.line(
+            self.image,
+            (dot3[0], dot3[1]),
+            (dot1[0], dot1[1]),
+            color=(255, 255, 255),
+            thickness=1,
+        )
+        #### ADD LINES ####
+
+        pts1 = np.float32(
+            [dot1, dot2, dot3, dot4]
+        )  # [[15,60],[5,130],[180,60],[190,130]]
         pts2 = np.float32([[0, 0], [0, 300], [300, 0], [300, 300]])
         M = cv2.getPerspectiveTransform(pts1, pts2)
         # editted_img_size = (editted_tmp_img.shape[1], editted_tmp_img.shape[0])
-        self.editted_img = cv2.warpPerspective(editted_tmp_img, M, (300, 300))  # img_size
-        self.editted_img_hsv = cv2.cvtColor(self.editted_img, cv2.COLOR_BGR2HSV)    
+        self.editted_image = cv2.warpPerspective(
+            editted_tmp_img, M, (300, 300)
+        )  # img_size
+        self.editted_image_hsv = cv2.cvtColor(self.editted_image, cv2.COLOR_BGR2HSV)
+        ########################################
+
         self.image_hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
         self.update_color()
-        
 
     def update_color(self):
         self.lower = np.array([self.lhue, self.lsat, self.lval])
         self.upper = np.array([self.hhue, self.hsat, self.hval])
         self.mask = cv2.inRange(self.image_hsv, self.lower, self.upper)
-        self.res = cv2.cvtColor(
+        self.image_res = cv2.cvtColor(
             cv2.bitwise_and(self.image_hsv, self.image_hsv, mask=self.mask),
             cv2.COLOR_HSV2BGR,
         )
         self.ref_image_mask = cv2.inRange(self.ref_image_hsv, self.lower, self.upper)
         self.ref_image_res = cv2.cvtColor(
-            cv2.bitwise_and(self.ref_image_hsv, self.ref_image_hsv, mask=self.ref_image_mask),
+            cv2.bitwise_and(
+                self.ref_image_hsv, self.ref_image_hsv, mask=self.ref_image_mask
+            ),
             cv2.COLOR_HSV2BGR,
         )
         #######=====CHECK+++ROI====######
-        lower_red = np.array([0,223,123])
-        upper_red = np.array([17,255,255])
-        lower_red2 = np.array([240,42,96])
-        upper_red2 = np.array([255,255,255])
-        
-        maskr = cv2.inRange(self.editted_img_hsv, lower_red, upper_red)
-        maskr2 = cv2.inRange(self.editted_img_hsv, lower_red2, upper_red2)
+        lower_red = np.array([self.lhue, self.lsat, self.lval])
+        upper_red = np.array([self.hhue, self.hsat, self.hval])
+        lower_red2 = np.array([240, 42, 96])
+        upper_red2 = np.array([255, 255, 255])
+
+        maskr = cv2.inRange(self.editted_image_hsv, lower_red, upper_red)
+        maskr2 = cv2.inRange(self.editted_image_hsv, lower_red2, upper_red2)
         maskr4 = cv2.bitwise_or(maskr2, maskr)
-		# filter mask
+        # filter mask
         kernel = np.ones((7, 7), np.uint8)
         opening = cv2.morphologyEx(maskr4, cv2.MORPH_OPEN, kernel)
         rgb_yb2 = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-        out_img = rgb_yb2.copy()
-        h, w = out_img.shape
-        search_top = int(1*h/4+20)
-        search_bot = int(3*h/4+20)
-        search_mid = int(w/2)
-        out_img[0:search_top, 0:w] = 0
-        out_img[search_bot:h, 0:w] = 0
+        self.editted_image_res = rgb_yb2.copy()
+        h, w = self.editted_image_res.shape
+        search_top = int(1 * h / 4 + 20)
+        search_bot = int(3 * h / 4 + 20)
+        search_mid = int(w / 2)
+        self.editted_image_res[0:search_top, 0:w] = 0
+        self.editted_image_res[search_bot:h, 0:w] = 0
         #######=====CHECK+++ROI====######
         return
 
@@ -155,20 +224,19 @@ class ROI_C:
         cv2.createTrackbar("hHue", "hsv filter trackbar", 0, 255, self.hhue_control)
         cv2.createTrackbar("hSat", "hsv filter trackbar", 0, 255, self.hsat_control)
         cv2.createTrackbar("hVal", "hsv filter trackbar", 0, 255, self.hval_control)
-        
+
         rate = rospy.Rate(10)  # Set a loop rate (10 Hz in this example)
         while not rospy.is_shutdown():
-        # 사용자 입력 대기 (키보드 입력을 대기하고 창을 닫을 때까지 대기)
-        
+            # 사용자 입력 대기 (키보드 입력을 대기하고 창을 닫을 때까지 대기)
+
             # 이미지를 화면에 표시
-            cv2.imshow("image", self.image)
-            cv2.imshow("mask", self.mask)
-            cv2.imshow("result", self.res)
             cv2.imshow("hsv filter trackbar", self.ref_image_res)
-            cv2.imshow("editted image",self.editted_image)
-            
-            
-            
+            cv2.imshow("image", self.image)
+            # cv2.imshow("mask", self.mask)
+            cv2.imshow("result", self.image_res)
+            cv2.imshow("editted image", self.editted_image)
+            cv2.imshow("editted image result", self.editted_image_res)
+
             pixel_count = cv2.countNonZero(self.mask)
             total_pixel_count = self.image.shape[0] * self.image.shape[1]
             area_ratio = pixel_count / total_pixel_count
@@ -192,6 +260,12 @@ class ROI_C:
 
 if __name__ == "__main__":
     img = cv2.imread("img_src/hsv_table.png", cv2.IMREAD_COLOR)
-    color_filter_node = ROI_C(img)
-    image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, color_filter_node.image_callback, queue_size=1, buff_size=52428800)
+    color_filter_node = ROI_CHECK(img)
+    image_sub = rospy.Subscriber(
+        "/usb_cam/image_raw",
+        Image,
+        color_filter_node.image_callback,
+        queue_size=1,
+        buff_size=52428800,
+    )
     color_filter_node.run()
